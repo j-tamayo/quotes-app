279d4cd2f7cf63c0ea17e01ddf66440f2b8f0abb
junior.pena <jtamayodev@gmail.com>
2017-06-14 14:13:07 -0400

    Adding confirm modal to mark as a favorite a quote

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 import { Quote } from '../../data/quote.interface';
 
@@ -10,7 +10,10 @@ import { Quote } from '../../data/quote.interface';
 export class QuotesPage implements OnInit {
   quoteGroup: {category: string, quotes: Quote[], icon: string};
 
-  constructor(private navParams: NavParams) {
+  constructor(
+    private navParams: NavParams,
+    private alertCtrl: AlertController
+  ) {
 
   }
 
@@ -22,4 +25,29 @@ export class QuotesPage implements OnInit {
   //   this.quoteGroup = this.navParams.data;
   //   Add elvis operator (?) in template to use this approach
   // }
+
+  onAddToFavorite(selectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () => {
+            console.log('Ok');
+          }
+        },
+        {
+          text: 'No, I changed my mind',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelled!');
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
 }
