0c9a06341e61586a6f6f8ec66ec9eb3adf712df3
junior.pena <jtamayodev@gmail.com>
2017-06-14 14:02:31 -0400

    Listing quotes

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,24 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the QuotesPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-@IonicPage()
+import { Quote } from '../../data/quote.interface';
+
 @Component({
   selector: 'page-quotes',
   templateUrl: 'quotes.html',
 })
-export class QuotesPage {
+export class QuotesPage implements OnInit {
+  quoteGroup: {category: string, quotes: Quote[], icon: string};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private navParams: NavParams) {
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotesPage');
+  ngOnInit() {
+    this.quoteGroup = this.navParams.data;
   }
 
+  // ionViewDidLoad() {
+  //   this.quoteGroup = this.navParams.data;
+  //   Add elvis operator (?) in template to use this approach
+  // }
 }
